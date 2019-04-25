ae2cce41bc3a8925823f437b22b1f334406e9597
Jonathan Scott <j301scott@gmail.com>
2019-04-25 14:18:49 -0700

    .embed file done

--- DisruptEditor/embedFile.cpp
+++ DisruptEditor/embedFile.cpp
@@ -2,23 +2,43 @@
 
 #include "IBinaryArchive.h"
 #include <SDL_assert.h>
+#include "Serialization.h"
 
 void embedFile::open(IBinaryArchive & fp) {
-	uint32_t magic = 2921855948;
+	fp.padding = fp.PADDING_NONE;
+
+	uint32_t magic = 0xAE27FBCC;
 	fp.serialize(magic);
-	SDL_assert_release(magic == 2921855948);
+	SDL_assert_release(magic == 0xAE27FBCC);
 
 	//If 0xAE27FBCC, then CEmbeddedResourceContainer::ProcessEmbbeddedData((void *,int))
+	if (magic == 0xAE27FBCC) {
+		fp.serializeNdVectorExternal(unk1);
+		db.open(fp);
+		SDL_assert_release(fp.tell() == fp.size());
+	}
+
 	//If 0xA2363453, then CEmbeddedResourceContainer::ProcessVectorRessourceData((void *,int))
+	//Unused
+}
 
-	//ProcessEmbbeddedData((void *,int))
-	uint32_t unk1;
+void embedFile::registerMembers(MemberStructure& ms) {
+	REGISTER_MEMBER(unk1);
+	REGISTER_MEMBER(db);
+}
+
+void embedFile::Unk1::read(IBinaryArchive& fp) {
 	fp.serialize(unk1);
-
-	// CResourceDataBase::LoadBinaryFile((IFile *,true))
-	uint32_t unk2;
 	fp.serialize(unk2);
+	fp.serialize(unk3);
+	fp.serialize(unk4);
+	fp.serialize(unk5);
+}
 
-	//Checks true or false from params
-	//Compares unk2 to r26 which is 0
+void embedFile::Unk1::registerMembers(MemberStructure& ms) {
+	REGISTER_MEMBER(unk1);
+	REGISTER_MEMBER(unk2);
+	REGISTER_MEMBER(unk3);
+	REGISTER_MEMBER(unk4);
+	REGISTER_MEMBER(unk5);
 }
--- DisruptEditor/embedFile.h
+++ DisruptEditor/embedFile.h
@@ -1,10 +1,25 @@
 #pragma once
 
+#include "CResourceDataBase.h"
+
 class IBinaryArchive;
+class MemberStructure;
 
 class embedFile {
 public:
 	void open(IBinaryArchive &fp);
+	void registerMembers(MemberStructure& ms);
 
+	struct Unk1 {
+		uint32_t unk1;
+		uint32_t unk2;
+		uint32_t unk3;
+		uint32_t unk4;
+		uint16_t unk5;
+		void read(IBinaryArchive& fp);
+		void registerMembers(MemberStructure& ms);
+	};
+	Vector<Unk1> unk1;
+	CResourceDataBase db;
 };
 
