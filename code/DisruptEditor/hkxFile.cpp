fea207cecb8c82868e451111c0ad3101dbc6f174
Jonathan Scott <j301scott@gmail.com>
2020-02-09 02:07:10 -0800

    havok WIP

--- code/DisruptEditor/hkxFile.cpp
+++ code/DisruptEditor/hkxFile.cpp
@@ -33,61 +33,81 @@ void hkxFile::read(IBinaryArchive& fp) {
 	fp.padding = fp.PADDING_NONE;
 	size_t beginOffset = SDL_RWtell(fp.fp);
 
-	fp.serializeConstant<uint32_t>(1474355287);
-	fp.serializeConstant<uint32_t>(281067536);
+	fp.serializeConstant<uint32_t>(1474355287);//magic1
+	fp.serializeConstant<uint32_t>(281067536);//magic2
 
-	fp.serialize(unk1);
-	fp.serializeConstant<uint32_t>(0x09);
+	fp.serialize(userTag);//userTag
+	fp.serializeConstant<uint32_t>(0x09);//fileVersion
 
 	//This is for x64 pc
+	//layoutRules
 	fp.serializeConstant<uint8_t>(8);//Trying to process a binary file with a different pointer size than this platform.
 	fp.serializeConstant<uint8_t>(1);//Trying to process a binary file with a different endian than this platform.
 	fp.serializeConstant<uint8_t>(0);//Trying to process a binary file with a different padding optimization than this platform.
 	fp.serializeConstant<uint8_t>(1);//Trying to process a binary file with a different empty base class optimization than this platform.
 
-	fp.serializeConstant<uint32_t>(3);//Number of sections, hardcoded to 3
-	fp.serializeConstant<uint32_t>(2);
-	fp.serializeConstant<uint32_t>(0);
-	fp.serializeConstant<uint32_t>(0);
-	fp.serializeConstant<uint32_t>(75);
+	fp.serializeConstant<uint32_t>(sections.size());//numSections, hardcoded
+	fp.serializeConstant<uint32_t>(2);//contentsSectionIndex
+	fp.serializeConstant<uint32_t>(0);//contentsSectionOffset
+	fp.serializeConstant<uint32_t>(0);//contentsClassNameSectionIndex
+	fp.serializeConstant<uint32_t>(75);//contentsClassNameSectionOffset
 
+	//contentsVersion, 16 bytes
 	const char* versionStr = "hk_2012.2.0-r1";
 	for (const char* it = versionStr; *it != '\0'; ++it)
 		fp.serializeConstant(*it);
 	fp.serializeConstant<uint8_t>(0);//Null term
-
-	//pad this to the next 4bytes
 	fp.serializeConstant<uint8_t>(0xFF);
 
+	//pad
 	fp.serializeConstant<uint32_t>(0);
 	fp.serializeConstant<uint32_t>(0xFFFFFFFF);
-	//This is the end of the 64 byte header
+	//This is the end of the 64 byte header, hkPackfileHeader
 
-	sections.resize(3);
+	//Read Sections
 	for(size_t i = 0; i < sections.size(); ++i)
 		fp.serialize(sections[i]);
 
 	//Read the first section, should be __classnames__
-	SDL_assert_release(sections[0].offsets[0] == SDL_RWtell(fp.fp) - beginOffset);
-	Sint64 mark = SDL_RWtell(fp.fp) + sections[0].offsets[1];
-	while(SDL_RWtell(fp.fp) < mark) {
+	SDL_assert_release(sections[CLASSNAMES].absoluteDataStart == SDL_RWtell(fp.fp) - beginOffset);
+	Sint64 mark = SDL_RWtell(fp.fp) + sections[CLASSNAMES].endOffset;
+	while(SDL_RWtell(fp.fp) < mark - 5) {
 		auto& it = classNames.emplace_back();
 		fp.serialize(it);
 	}
+	//Read Pad
+	for (int i = 0; i < 5; ++i)
+		fp.serializeConstant<uint8_t>(0xFF);
 
+	//Make sure __types__ is empty
+	SDL_assert_release(sections[TYPES].localFixupsOffset == 0);
+	SDL_assert_release(sections[TYPES].globalFixupsOffset == 0);
+	SDL_assert_release(sections[TYPES].virtualFixupsOffset == 0);
+	SDL_assert_release(sections[TYPES].exportsOffset == 0);
+	SDL_assert_release(sections[TYPES].importsOffset == 0);
+	SDL_assert_release(sections[TYPES].endOffset == 0);
 
+	//Read __data__
+	SDL_assert_release(sections[DATA].absoluteDataStart == SDL_RWtell(fp.fp) - beginOffset);
+
+	__debugbreak();
 }
 
 void hkxFile::Section::read(IBinaryArchive& fp) {
-	fp.memBlock(name.data(), 1, name.size());
-	fp.serializeConstant<uint32_t>(0xFF000000);
+	fp.memBlock(sectionTag.data(), 1, sectionTag.size());
+	fp.serializeConstant<uint8_t>(0xFF);//nullByte
 
-	for (size_t i = 0; i < offsets.size(); ++i)
-		fp.serialize(offsets[i]);
+	fp.serialize(absoluteDataStart);
+	fp.serialize(localFixupsOffset);
+	fp.serialize(globalFixupsOffset);
+	fp.serialize(virtualFixupsOffset);
+	fp.serialize(exportsOffset);
+	fp.serialize(importsOffset);
+	fp.serialize(endOffset);
 }
 
 void hkxFile::ClassNames::read(IBinaryArchive& fp) {
-	fp.serialize(unk1);
+	fp.serialize(signature);
 	fp.serializeConstant<uint8_t>(9);
 	serializeHkString(fp, name);
 }
--- code/DisruptEditor/hkxFile.h
+++ code/DisruptEditor/hkxFile.h
@@ -5,21 +5,239 @@
 
 class IBinaryArchive;
 
+//Size 0xc
+template <typename T>
+struct hkArray {
+	T* data;
+	int size;
+	int capacityAndFlags;// highest 2 bits indicate any special considerations about the allocation for the array
+
+	enum {
+		CAPACITY_MASK = int(0x3FFFFFFF),
+		FLAG_MASK = int(0xC0000000),
+		DONT_DEALLOCATE_FLAG = int(0x80000000), // Indicates that the storage is not the array's to delete
+		ALLOCATED_FROM_SPU = int(0x40000000),	// Ps3 specific. Indicates that the array storage has been allocated as a result of a SPU request.
+		FORCE_SIGNED = -1
+	};
+};
+
+//Size: 0x4, inherits: none
+struct hkBaseObject {
+	//no members
+private:
+	uint32_t baseStub;//Just to pad to 0x4
+};
+
+//Size: 0x8, inherits: hkBaseObject
+struct hkReferencedObject : public hkBaseObject {
+	uint16_t memSizeAndFlags;//0x4
+	int16_t referenceCount;//0x6
+};
+
+/*//Size: 0x8, inherits: none
+struct hkpEntitySmallArraySerializeOverrideType {
+	void* data;//0x0
+	uint16_t size;//0x4
+	uint16_t capacityAndFlags;//0x6
+};
+
+//Size: 0x38, inherits hkReferencedObject
+struct hkpConstraintInstance : public hkReferencedObject {
+
+};
+
+//Size: 0x220, inherits: 
+struct hkpEntity {
+	hkpMaterial material;//0x88
+	void* limitContactImpulseUtilAndFlag;//0x94, SERIALIZE_IGNORED
+	float damageMultiplier;//0x98
+	void* breakableBody;//0x9C, SERIALIZE_IGNORED
+	uint32_t solverData;//0xA0, SERIALIZE_IGNORED
+	uint16_t storageIndex;//0xA4
+	uint16_t contactPointCallbackDelay;//0xA6
+	hkpEntitySmallArraySerializeOverrideType constraintsMaster;//0xA8, SERIALIZE_IGNORED
+	hkArray<hkConstraintInternal> constraintsSlave;//0xB0, SERIALIZE_IGNORED | NOT_OWNED
+	hkArray<uint8_t> constraintRuntime;//0xBC, SERIALIZE_IGNORED
+};
+
+//Size: 0x220, inherits: hkpEntity
+struct hkpRigidBody : public hkpEntity {
+	//no members
+};*/
+
+//Size: 0x10, inherits: none
+struct SHkChildSpawnableMergedInstanceInfos {
+	uint32_t type;//0x0
+	uint32_t batchedInstanceIdOrPhysInstanceHandle;//0x4
+	uint32_t entityId;//0x8
+};
+
+//Size: 0x6, inherits: none
+struct CHkPhysMergedInstance {
+	uint16_t resourceIdx; //0x0
+	uint16_t startCompoundInstanceIdx; //0x2
+	uint16_t type; //0x4
+};
+
+//Size: 0x6, inherits: CHkPhysMergedInstance
+struct CHkPhysStaticMergedInstance : public CHkPhysMergedInstance {
+	//No Members
+};
+
+//Size: 0x1c, inherits: CHkPhysMergedInstance
+struct CHkPhysSpawnableMergedInstance : public CHkPhysMergedInstance {
+	bool removed;//0x6
+	uint32_t instanceID;//0x8
+	uint32_t linkedInstanceInfoStartIndex;//0xc
+	uint32_t linkedInstanceInfoCount;//0x10
+	float health;//0x14
+	void* dynamicLinkedEntities;//0x18, SERIALIZE_IGNORED
+};
+
+//Size: 0x1c, inherits: CHkPhysSpawnableMergedInstance
+struct CHkPhysDynamicMergedInstance : public CHkPhysSpawnableMergedInstance {
+	//No Members
+};
+
+//Size: 0x1c, inherits: CHkPhysSpawnableMergedInstance
+struct CHkPhysBreakableMergedInstance : public CHkPhysSpawnableMergedInstance {
+	//No Members
+};
+
+//Size: 0xc, inherits: none
+struct CHkPhysMergedResource {
+	uint32_t resourceId;//0x0
+	void* physResourceImpl;//0x4, SERIALIZE_IGNORED
+	uint16_t typeFlags;//0x8
+};
+
+//Size: 0xC, inherits: CHkPhysMergedResource
+struct CHkPhysSpawnableMergedResource : public CHkPhysMergedResource {
+	//No Members
+};
+
+//Size: 0x30, inherits: CHkPhysSpawnableMergedResource
+struct CHkPhysDynamicMergedResource : public CHkPhysSpawnableMergedResource {
+	float damageScales[7];//0xC
+	float overrideMass;//0x28
+	uint8_t damageFlags;//0x2C
+	bool staticFromStart;//0x2D
+};
+
+//Size: 0x80, inherits: none
+struct SHkPhysBreakableInitialMotionParams {
+	glm::vec4 towardsCameraTargets;//0x0
+	glm::vec4 directionalMinVel;//0x10
+	glm::vec4 directionalMaxVel;//0x20
+	glm::vec4 rotationalMinVel;//0x30
+	glm::vec4 rotationalMaxVel;//0x40
+	glm::vec4 radialCenterOffset;//0x50
+	float radialMinSpeed;//0x60
+	float radialMaxSpeed;//0x64
+	float towardsCameraMinSpeed;//0x68
+	float towardsCameraMaxSpeed;//0x6C
+	uint8_t flags;//0x70
+	uint8_t breakerTypeFlags;//0x71
+	bool directionalLocalSpace;//0x72
+	bool rotationalLocalSpace;//0x73
+};
+
+//Size: 0x80, inherits: none
+struct SHkPhysBreakableLayerParamsData {
+	float health;//0x0
+	float autoDestructionTriggerHealthFactor;//0x4
+	float needUpdateHealthFactor;//0x8
+	float destructionTimeMin;//0xC
+	float destructionTimeMax;//0x10
+	float linearDamping;//0x14
+	float angularDamping;//0x18
+	float constraintFrictionFactor;//0x1C
+	float constraintHealthFactor;//0x20
+	float constraintBreachImpulseFactor;//0x24
+	float collisionVelocityTransferFactor;//0x28
+	uint16_t onLeafDetachBehaviour;//0x2C
+	uint8_t damageFlags;//0x2E
+	uint8_t flags;//0x2F
+	float damageScales[7];//0x30
+	float constraintDamageScales[7];//0x4C
+	hkArray<uint16_t> parts;//0x68
+	hkArray<SHkPhysBreakableInitialMotionParams> initialMotionParams;//0x74
+};
+
+//Size: 0x2c, inherits: CHkPhysSpawnableMergedResource
+struct CHkPhysBreakableMergedResource : public CHkPhysSpawnableMergedResource {
+	float density;//0xc
+	float explosionImpulseFactor;//0x10
+	float explosionRandomFactor;//0x14
+	float linkedChildsDamageOnSpawn;//0x18
+	bool linkedChildsSwitchStateOnSpawn;//0x1C
+	hkArray<SHkPhysBreakableLayerParamsData> layerParams;//0x20
+};
+
+class hkpRigidBody;
+
+//Size: 0x90, inherits: hkReferencedObject
+struct CHkPhysMergedBody : public hkReferencedObject {
+	hkpRigidBody *rigidBody;  //0x8, struct ptr
+	hkArray<SHkChildSpawnableMergedInstanceInfos> childInstanceInfos;  //0xC, struct array
+	hkArray<CHkPhysStaticMergedInstance> staticInstances; //0x18, struct array
+	hkArray<CHkPhysDynamicMergedInstance> dynamicInstances; //0x24, struct array
+	hkArray<CHkPhysBreakableMergedInstance> breakableInstances; //0x30, struct array
+	hkArray<CHkPhysMergedResource> staticMergedResources; //0x3C, struct array
+	hkArray<CHkPhysDynamicMergedResource> dynamicMergedResources; //0x48, struct array
+	hkArray<CHkPhysBreakableMergedResource> breakableMergedResources; //0x54, struct array
+	uint32_t version; //0x60
+};
+
 class hkxFile {
 public:
-	uint32_t unk1;
+	int32_t userTag;
 
 	struct Section {
-		std::array<char, 16> name;
+		std::array<char, 19> sectionTag;
 
-		std::array<uint32_t, 7> offsets;
+		int32_t absoluteDataStart;
+
+		//Add absoluteDataStart to these
+		int32_t localFixupsOffset;
+		int32_t globalFixupsOffset;
+		int32_t virtualFixupsOffset;
+		int32_t exportsOffset;
+		int32_t importsOffset;
+		int32_t endOffset;
+
+		/// Size in bytes of data part
+		int getDataSize() const {
+			return localFixupsOffset;
+		}
+		/// Size in bytes of intra section pointer patches
+		int getLocalSize() const {
+			return globalFixupsOffset - localFixupsOffset;
+		}
+		/// Size in bytes of inter section pointer patches
+		int getGlobalSize() const {
+			return virtualFixupsOffset - globalFixupsOffset;
+		}
+		/// Size in bytes of finishing table.
+		int getFinishSize() const {
+			return exportsOffset - virtualFixupsOffset;
+		}
+		/// Size in bytes of exports table.
+		int getExportsSize() const {
+			return importsOffset - exportsOffset;
+		}
+		/// Size in bytes of imports table.
+		int getImportsSize() const {
+			return endOffset - importsOffset;
+		}
 
 		void read(IBinaryArchive& fp);
 	};
-	std::vector<Section> sections;
+	std::array<Section, 3> sections;
+	enum SectionTypes { CLASSNAMES = 0, TYPES = 1, DATA = 2 };
 
 	struct ClassNames {
-		uint32_t unk1;
+		uint32_t signature;
 		std::string name;
 		void read(IBinaryArchive& fp);
 	};
